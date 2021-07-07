9266cfda63b603085594a9b9892d8076ba6589fb
ClarkHo <593628543@qq.com>
2021-07-07 08:52:32 +0800

    增加压缩

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import { uglify } from 'rollup-plugin-uglify';
+
 export default {
     input: 'src/main.js',
     output: {
         file: 'bundle.js',
         format: 'umd',
         name: 'clark'
-    }
+    },
+    plugins: [
+        uglify()
+    ]
 }
